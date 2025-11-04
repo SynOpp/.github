8027baed51c8cc8cf07426812627fe452968e7c1
MatsM16 <matdyr23@gmail.com>
2025-11-04 21:46:20 +0100

    Attempt at fancy home page

---
+++ docs/index.js
@@ -0,0 +1,60 @@
+const pointsPerPixel = 0.0004;
+const minDistance = 50;
+const maxDistance = 100;
+
+const canvas = document.querySelector('canvas');
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.scrollHeight;
+const ctx = canvas.getContext('2d');
+
+const pointCount = pointsPerPixel * canvas.width * canvas.height;
+console.log(`Creating ${pointCount} points`);
+const points = [];
+
+for (let i = 0; i < pointCount; i++) {
+    points.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        vx: (Math.random() - 0.5) * 0.5,
+        vy: (Math.random() - 0.5) * 0.5
+    });
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    points.forEach(point => {
+        point.x += point.vx;
+        point.y += point.vy;
+
+        if (point.x < 0) point.x = 0;
+        if (point.x > canvas.width) point.x = canvas.width;
+        if (point.y < 0) point.y = 0;
+        if (point.y > canvas.height) point.y = canvas.height;
+
+        if (point.x == 0 || point.x == canvas.width) point.vx *= -1;
+        if (point.y == 0 || point.y == canvas.height) point.vy *= -1;
+
+        ctx.fillStyle = '#0078D7'
+        ctx.strokeStyle = '#0078D7'
+        // ctx.beginPath();
+        // ctx.arc(point.x, point.y, 2, 0, Math.PI * 2);
+        // ctx.fill();
+
+        points.forEach(other => {
+            if (point === other) return;
+            const dx = point.x - other.x;
+            const dy = point.y - other.y;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+
+            if (minDistance < dist && dist < maxDistance) {
+                ctx.beginPath();
+                ctx.moveTo(point.x, point.y);
+                ctx.lineTo(other.x, other.y);
+                ctx.stroke();
+            }
+        });
+    });
+    requestAnimationFrame(update);
+}
+
+update();
